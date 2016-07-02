d25c17f7c42f4c2e2123e638d8f933d355416a92
destinysync <destinysync@hotmail.com>
2016-07-02 14:15:13 +0000

    0.0.1

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use(passport.session());
 
 routes(app, passport);
 
-var port = 8080;
+var port = process.env.PORT || 8080;
 app.listen(port,  function () {
 	console.log('Node.js listening on port ' + port + '...');
 });
